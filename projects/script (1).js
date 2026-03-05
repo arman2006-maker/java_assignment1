a59549f161abc10a2c7fe1dac75372d69c0c1cf1
arman kumar <armanprince2006@gmail.com>
2026-03-05 12:12:58 +0530

    Add files via upload

---
+++ projects/script (1).js
@@ -0,0 +1,65 @@
+let events = [];
+function addEvent() {
+  let title = document.getElementById("title").value;
+  let date = document.getElementById("date").value;
+  let category = document.getElementById("category").value;
+  let desc = document.getElementById("description").value;
+
+  if (title === "" || date === "") {
+    alert("Please fill all required fields!");
+    return;
+  }
+
+  let event = { title, date, category, desc };
+  events.push(event);
+
+  displayEvents();
+  clearForm();
+}
+
+
+function displayEvents() {
+  let list = document.getElementById("eventList");
+  list.innerHTML = "";
+
+  if (events.length === 0) {
+    list.innerHTML = "No events yet!";
+    return;
+  }
+
+  for (var i = 0; i < events.length; i++) {
+    var e = events[i];
+
+    list.innerHTML = list.innerHTML + 
+        "<div class='event'>" +
+        "<b>" + e.title + "</b><br>" +
+        "📅 " + e.date + " | " + e.category + "<br>" +
+        e.desc +
+        "</div>";}
+}
+
+function clearForm() {
+  document.getElementById("title").value = "";
+  document.getElementById("date").value = "";
+  document.getElementById("description").value = "";
+}
+
+function clearEvents() {
+  events = [];
+  displayEvents();
+}
+
+function addSample() {
+  events.push({
+    title: "AI Workshop",
+    date: "2026-02-20",
+    category: "Workshop",
+    desc: "Hands-on AI tools training"
+  });
+  displayEvents();
+}
+
+document.addEventListener("keydown", function(e) {
+  document.getElementById("keyOutput").innerText =
+    "You Pressed: " + e.key;
+});
